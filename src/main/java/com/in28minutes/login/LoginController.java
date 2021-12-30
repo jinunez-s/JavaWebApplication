9f44f1575cb9e2005437faac68e57f822007c7f6
Julio Nuñez HP <julionr14@gmail.com>
2021-12-29 18:04:43 -0600

    Updating files

---
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -0,0 +1,32 @@
+package com.in28minutes.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private LoginService loginservice;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)	
+	public String showLoginPage() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)	
+	public String handleLoginRequest(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		
+		if(!loginservice.validateUser(name, password)) {
+			model.put("errorMessage", "Invalid Credentials!");
+			return "login";
+		}		
+		model.put("name", name);
+		return "welcome";				
+	}
+}
